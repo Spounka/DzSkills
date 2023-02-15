3df268e08eee0903ec9f484d3ff6640f5b52eadc
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2023-02-15 10:06:47 +0100

    added dotenv configs

--- backend/settings.py
+++ backend/settings.py
@@ -9,24 +9,29 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os.path
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+if not os.path.isfile(BASE_DIR / '.env'):
+    raise RuntimeError('No dotenv found, please create one')
+
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x63(ghab0!cz^x1*)3y6)uu9#)%jva81aq9tz&*n1-!1f08w!('
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', False)
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
