24a2b5df521296b5b8fd1841e50a6f4864611d0e
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2023-02-15 10:33:30 +0100

    changed host in settings.py to simplify debugging

--- backend/settings.py
+++ backend/settings.py
@@ -146,7 +146,7 @@ DATABASES = {
         'NAME':     os.environ.get('POSTGRES_DB', os.environ.get('DB', '')),
         'USER':     os.environ.get('POSTGRES_USER', os.environ.get('DB_USER', '')),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD', os.environ.get('DB_PASS', '')),
-        'HOST':     'db',
+        'HOST':     os.environ.get('db', os.environ.get('DB_HOST', 'localhost')),
         'PORT':     5432
 
     }
