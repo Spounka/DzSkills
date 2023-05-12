e67099447ae9f967b955de097c620cbc99c7aca2
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2023-05-12 11:43:52 +0100

    Added category migration + field in the course model

--- courses/models.py
+++ courses/models.py
@@ -66,6 +66,8 @@ class Course(models.Model):
 
     course_level = models.CharField(max_length=30, choices=LEVEL_CHOICES, default=BEGINNER)
     level = models.ForeignKey(Level, on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+
     duration = models.CharField(max_length=10, default="1h")
     used_programs = models.CharField(max_length=300, default="")
     language = models.CharField(max_length=30, default="Arabic")
