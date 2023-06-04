3a8bcd2d4782dbae5272180d904b20734f484267
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2023-06-04 14:42:44 +0100

    flipped the debug variable to read from ENV instead

--- backend/settings.py
+++ backend/settings.py
@@ -30,8 +30,8 @@ load_dotenv()
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = (os.environ.get('DEBUG', True) == "true")
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG', True) == "true")
+# DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
