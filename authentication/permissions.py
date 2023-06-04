24086cbdc9d1878cb87cf9be00f86192e7e57bf4
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2023-06-04 14:42:19 +0100

    started adding permissions to control the application

--- authentication/permissions.py
+++ authentication/permissions.py
@@ -1,6 +1,14 @@
+from django.contrib.auth.models import Group
 from rest_framework.permissions import BasePermission
 
 
-class IsAdminOrEdittingSelf(BasePermission):
+class IsSuperUser(BasePermission):
     def has_permission(self, request, view):
-        return bool(request.user and request.user.is_superuser)
+        return request.user.is_superuser or request.user.is_staff
+
+
+class IsAdmin(BasePermission):
+    group_name = "AdminGroup"
+
+    def has_permission(self, request, view):
+        return request.user.groups.filter(name=self.group_name).exists()
